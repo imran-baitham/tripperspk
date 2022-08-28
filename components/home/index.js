4de3cd9313b8989b35e0523af2bff9cb3de13c05
imran-baitham <imranbaitham001@gmail.com>
2022-08-28 20:00:57 +0500

    resolve conflit

--- components/home/index.js
+++ components/home/index.js
@@ -3,9 +3,9 @@ const Dreams = () => {
   return (
     <div>
     <div
-        class="w-full h-[90vh] bg-[url('https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg')] bg-cover bg-center">
-        <div class="w-full h-full flex flex-col justify-center  backdrop-blur-lg">
-            <h1 class="mt-5  text-4xl text-white ml-[8%] leading-[100px] text-6xl font-semibold drop-shadow-lg bold">Bring your dreams <br/> to life with locals</h1>
+        className="w-full h-[90vh] bg-[url('https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg')] bg-cover bg-center">
+        <div className="w-full h-full flex flex-col justify-center  backdrop-blur-lg">
+            <h1 className="mt-5  text-4xl text-white ml-[8%] leading-[100px] text-6xl font-semibold drop-shadow-lg bold">Bring your dreams <br/> to life with locals</h1>
         </div>
     </div>
     </div>
