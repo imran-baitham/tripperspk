1b0e055941a70eb3648b16b3a87b6a75f0a30c61
imran-baitham <imranbaitham001@gmail.com>
2022-08-28 22:20:24 +0500

    completed category

---
+++ components/category/category.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { BsChevronDown } from "react-icons/bs";
+
+/* This example requires Tailwind CSS v2.0+ */
+const navigation = [
+    { name: 'Cars', href: '#' },
+    { name: 'Motorcycle', href: '#' },
+    { name: 'house', href: '#' },
+    { name: 'lands and Plots', href: '#' },
+    { name: 'Apartments', href: '#' },
+    { name: 'Cameras', href: '#' },
+]
+
+function Category(props) {
+    return (
+        <div className="w-full h-auto w-5/6 m-auto text-white">
+            <h1 className="font-black text-black text-5xl leading-[55px] text-center pt-14">
+                Find Rent your own choose
+            </h1>
+            <header className="bg-[#F4F4F4] text-black mt-10">
+                <nav aria-label="Top">
+                    <div className="w-full flex items-center justify-between border-b border-indigo-500 lg:border-none">
+                        <div className="flex items-center">
+                            <button className='font-semibold px-4 py-2 w-64 text-left flex items-center justify-between focus:bg-gray-200'>All Products <BsChevronDown /></button>
+                            <div className="hidden ml-10 space-x-8 lg:block">
+                                {navigation.map((link) => (
+                                    <a key={link.name} href={link.href} className="text-base font-medium text-black">
+                                        {link.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+                        {navigation.map((link) => (
+                            <a key={link.name} href={link.href} className="text-base font-medium text-black hover:text-indigo-50">
+                                {link.name}
+                            </a>
+                        ))}
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default Category;
+
+
--- components/home/index.js
+++ components/home/index.js
@@ -2,12 +2,15 @@ import React from "react";
 const Dreams = () => {
   return (
     <div>
-    <div
-        className="w-full h-[90vh] bg-[url('https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg')] bg-cover bg-center">
+      <div
+        className="w-full h-[76vh] bg-[url('https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg')] bg-cover bg-center">
         <div className="w-full h-full flex flex-col justify-center  backdrop-blur-lg">
-            <h1 className="mt-5  text-4xl text-white ml-[8%] leading-[100px] text-6xl font-semibold drop-shadow-lg bold">Bring your dreams <br/> to life with locals</h1>
+          <h1 className="mt-5 text-4xl text-white ml-[8%] leading-[80px] text-6xl font-semibold drop-shadow-lg bold">Bring your dreams <br /> to life with locals</h1>
+          <div className="leading-[100px]">
+            <button className="text-white ml-[8%] text-lg font-bold bg-[#0047FF] py-3 px-7">Rent Now</button>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   )
 };
