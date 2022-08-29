403201fe126a68df52916b568761baa37b8b2b0b
“HaiderAbas” <haiderabbas.gb5@gmail.com>
2022-08-29 13:24:01 +0500

    product update

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,12 +1,9 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import testimage from "../../public/images/nature.jpg";
-
+import starsImg from "../../public/images/imageStar.png"
 export default function Product() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [people, setData] = useState(null);
   const getData = () => fetch("/api/project").then((res) => res.json());
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -17,20 +14,10 @@ export default function Product() {
   }, []);
 
   if (!people) return <div className="text-black text-3xl">loading...</div>;
-  // const skeletonMap = [0, 1, 2, 3, 4, 5, 6];
-  // if (!people)
-  //     return (
-  //         <div className="text-white text-3xl h-screen">
-  //             {skeletonMap.map(() => {
-  //                 // eslint-disable-next-line react/jsx-key
-  //                 return <h1>item parts</h1>;
-  //             })}
-  //         </div>
-  //     );
 
   return (
     <div className="w-full h-auto w-5/6 m-auto rounded-md">
-      <h1>Rents Nearby</h1>
+      <h1 className="flex mt-7 ml-10 font-bold items-center">Rents Nearby</h1>
       <ul
         role="list"
         className="p-10 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
@@ -52,19 +39,22 @@ export default function Product() {
               </div>
             </Link>
             <div className="text-left p-6">
-              <h1 className="text-2xl font-[400]">{person.name}</h1>
-              <p className="pt-1">{person.datails}</p>
-              <div className="pt-2 flex items-center justify-between">
+              <h3 className="text-2xl font-[300] font-semibold">{person.name}</h3>
+              <p className="pt-1 font-poppins">{person.datails}</p>
+              <div className="pt-2 flex items-center justify-between font-semibold">
                 <h2>{person.charge}</h2>
                 <h2>{person.price}</h2>
               </div>
-              <div className="pt-2 flex items-center justify-between">
-                <h2>{person.address}</h2>
+              <div className="pt-2 flex items-center justify-between  ">
+                <h2 >{person.address}</h2>
                 <h2>{person.time}</h2>
               </div>
               <div className="pt-2 flex items-center justify-between">
-                <h2>{person.rate}</h2>
-                <button className="bg-red-300 py-2 px-5 rounded-md">
+               <div className="flex items-center aligin-center">
+                <img className="w-[105px]" src={starsImg.src} alt="image not get from api"/>
+               <h2 className="ml-2">{person.rate}</h2>
+               </div>
+                <button className="bg-[#0047FF] text-white py-1 px-4 rounded-md">
                   {person.status === "available" ? "available" : "order it"}
                 </button>
               </div>
