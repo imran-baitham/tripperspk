98c7f4a1e0d365a41656689beb4486741a42b9a7
“HaiderAbas” <haiderabbas.gb5@gmail.com>
2022-08-30 02:31:19 +0500

    login-from-completed

--- components/category/category.js
+++ components/category/category.js
@@ -31,17 +31,18 @@ const Tour = [
   { name: "Apartments", href: "#" },
 ];
 
-
-
 function Category(props) {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   return (
     <div className="w-full h-auto w-5/6 m-auto text-white">
       <header className="bg-[#F4F4F4] text-black mt-10">
         <nav aria-label="Top">
           <div className="w-full flex items-center justify-between border-none">
             <div className="flex items-center">
-              <button className="font-semibold px-4 py-3 w-64 text-left flex items-center justify-between focus:bg-gray-200" onClick={() => setShow(!show)}>
+              <button
+                className="font-semibold px-4 py-3 w-64 text-left flex items-center justify-between focus:bg-gray-200"
+                onClick={() => setShow(!show)}
+              >
                 All Products <BsChevronDown />
               </button>
               <div className="hidden ml-10 space-x-8 lg:block">
@@ -70,11 +71,9 @@ function Category(props) {
           </div>
         </nav>
       </header>
-      {show &&
+      {show && (
         <div className="font-black text-black text-left absolute left-50 -bottom-30 z-40 bg-[#F4F4F4] mt-2">
-          <div
-            className="m-5 grid grid-cols-1 gap-5 sm:grid-cols-2"
-          >
+          <div className="m-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
             <div>
               <h2 className="text-2xl">Accomodations</h2>
               <ul>
@@ -141,7 +140,7 @@ function Category(props) {
             </div>
           </div>
         </div>
-      }
+      )}
     </div>
   );
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -14,7 +14,6 @@ import Category from "../../components/category/category";
 import Link from "next/link";
 import ChatBox from "../chatBox";
 
-
 function ProductScreen() {
   const [people, setData] = useState([]);
   const getData = () => fetch("/api/project").then((res) => res.json());
@@ -47,13 +46,25 @@ function ProductScreen() {
               className=""
             >
               <SwiperSlide>
-                <img className="h-[500px] bg-gray-200 w-full" src={person.productApiUrl} alt="" />
+                <img
+                  className="h-[500px] bg-gray-200 w-full"
+                  src={person.productApiUrl}
+                  alt=""
+                />
               </SwiperSlide>
               <SwiperSlide>
-                <img className="h-[500px] bg-gray-200 w-full" src={person.productApiUrl} alt="" />
+                <img
+                  className="h-[500px] bg-gray-200 w-full"
+                  src={person.productApiUrl}
+                  alt=""
+                />
               </SwiperSlide>
               <SwiperSlide>
-                <img className="h-[500px] bg-gray-200 w-full" src={person.productApiUrl} alt="" />
+                <img
+                  className="h-[500px] bg-gray-200 w-full"
+                  src={person.productApiUrl}
+                  alt=""
+                />
               </SwiperSlide>
             </Swiper>
           </main>
@@ -62,30 +73,37 @@ function ProductScreen() {
               <h1 className="font-black text-black text-3xl leading-[55px] text-left pt-2 pb-3">
                 Saller description
               </h1>
-              <div
-                className="relative flex items-center"
-              >
+              <div className="relative flex items-center">
                 <div>
-                  <img className="h-14 w-14 rounded-full mr-3" src={person.productApiUrl} alt="image" />
+                  <img
+                    className="h-14 w-14 rounded-full mr-3"
+                    src={person.productApiUrl}
+                    alt="image"
+                  />
                 </div>
                 <div className="flex-1 min-w-0">
                   <a href="#" className="">
                     <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                    <p className="text-sm text-gray-500 truncate">{person.member}</p>
+                    <p className="text-sm font-medium text-gray-900">
+                      {person.name}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate">
+                      {person.member}
+                    </p>
                   </a>
                 </div>
               </div>
               <div className="my-5 text-center">
-                
                 <Link href="/chatBox">
-                <button className="bg-[#0047FF] absolute bottom-2 left-2 text-white border-1 py-3 px-32 mt-10">Chat with saller</button>
+                  <button className="bg-[#0047FF] absolute bottom-2 left-2 text-white border-1 py-3 px-32 mt-10">
+                    Chat with saller
+                  </button>
                 </Link>
               </div>
             </div>
           </aside>
         </div>
-      </div >
+      </div>
 
       <div className="w-full h-auto w-5/6 m-auto rounded-md">
         <div className="flex-1 relative z-0 flex overflow-hidden my-10">
@@ -108,51 +126,67 @@ function ProductScreen() {
             <div className="my-10">
               <div className="relative flex items-center">
                 <div>
-                  <img className="h-12 w-12 rounded-full mr-3" src={person.productApiUrl} alt="image" />
+                  <img
+                    className="h-12 w-12 rounded-full mr-3"
+                    src={person.productApiUrl}
+                    alt="image"
+                  />
                 </div>
                 <div className="flex-1 min-w-0">
                   <a href="#" className="">
                     <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                    <p className="text-sm text-gray-500 truncate">{"helpful"}</p>
+                    <p className="text-sm font-medium text-gray-900">
+                      {person.name}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate">
+                      {"helpful"}
+                    </p>
                   </a>
                 </div>
               </div>
 
-              <div
-                className="relative flex items-center mt-4"
-              >
+              <div className="relative flex items-center mt-4">
                 <div>
-                  <img className="h-12 w-12 rounded-full mr-3" src={person.productApiUrl} alt="image" />
+                  <img
+                    className="h-12 w-12 rounded-full mr-3"
+                    src={person.productApiUrl}
+                    alt="image"
+                  />
                 </div>
                 <div className="flex-1 min-w-0">
                   <a href="#" className="">
                     <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="text-sm font-medium text-gray-900">{person.name}</p>
-                    <p className="text-sm text-gray-500 truncate">{"Nice work"}</p>
+                    <p className="text-sm font-medium text-gray-900">
+                      {person.name}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate">
+                      {"Nice work"}
+                    </p>
                   </a>
                 </div>
               </div>
             </div>
-            <input type={"text"} placeholder="Write your review" className={"mt-14 w-full border border-gray-300 p-4 outline-none"} />
+            <input
+              type={"text"}
+              placeholder="Write your review"
+              className={"mt-14 w-full border border-gray-300 p-4 outline-none"}
+            />
           </main>
           <aside className="relative xl:flex xl:flex-col flex-shrink-0 w-96 overflow-y-auto">
             <div className="border border-1 h-96">
-              <div
-                className="relative flex items-center"
-              >
+              <div className="relative flex items-center">
                 <div className="flex-1 min-w-0">
-                  <div dangerouslySetInnerHTML={{
-                    __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103408.16812933404!2d74.30191438079711!3d35.910131013989314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e649e3642543b1%3A0x40fd0ca3ca17282b!2sGilgit!5e0!3m2!1sen!2s!4v1661798028243!5m2!1sen!2s" width="100%" height="380" style="border:0;" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`
-                  }} >
-
-                  </div>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103408.16812933404!2d74.30191438079711!3d35.910131013989314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38e649e3642543b1%3A0x40fd0ca3ca17282b!2sGilgit!5e0!3m2!1sen!2s!4v1661798028243!5m2!1sen!2s" width="100%" height="380" style="border:0;" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`,
+                    }}
+                  ></div>
                 </div>
               </div>
             </div>
           </aside>
         </div>
-      </div >
+      </div>
     </>
   );
 }
