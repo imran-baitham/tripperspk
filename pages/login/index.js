5e47ef1e83dc923d882600d87f0ca8a5616700d5
“HaiderAbas” <haiderabbas.gb5@gmail.com>
2022-08-31 16:49:00 +0500

    validation-completed

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import img from "../../public/images/logo.png";
 import Link from "next/link";
+import { useRouter } from "next/router";
 function LoginFrom() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailerror, setEmailError] = useState("");
+  const [passworderror, setPasswordError] = useState("");
+  const Validation = () => {
+    const errors = {};
+    if (email === "") errors.email = "email is required";
+
+    if (password === "") errors.password = "password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+  const hendleSubmit = (e) => {
+    const errors = Validation();
+    if (errors) {
+      setEmailError(errors.email);
+      setPasswordError(errors.password);
+    } else {
+      router.push("/")
+      setPassword("");
+      setEmail("");
+      setEmailError(email = "");
+      setPasswordError(password = "");
+    }
+    e.preventDefault();
+  };
+
   return (
     <div>
       <div className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white  shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <div className="flex justify-center my-14">
-                <Image src={img} alt="image" />
-              </div>
+            <form className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="flex justify-center my-14">
+                  <Image src={img} alt="image" />
+                </div>
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
@@ -19,20 +48,27 @@ function LoginFrom() {
                 <div>
                   <label
                     type="email"
+                    htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Email
                   </label>
                   <input
                     type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required=""
                   />
+                  <div className="block mb-2 text-sm font-medium text-red-900 dark:text-white">
+                    {emailerror}
+                  </div>
                 </div>
                 <div>
                   <label
                     type="password"
+                    htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Password
@@ -40,11 +76,16 @@ function LoginFrom() {
                   <input
                     type="password"
                     name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     id="password"
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
+                  <div className="block mb-2  text-sm font-medium text-red-900 dark:text-white">
+                    {passworderror}
+                  </div>
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
@@ -65,11 +106,12 @@ function LoginFrom() {
                   </div>
                 </div>
                 <Link href={"/"}>
-                  <button
-                    className="w-full font-medium text-white bg-[#0047FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium  text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                  >
-                    Sign in
-                  </button>
+                <button
+                  onClick={(event) => hendleSubmit(event)}
+                  className="w-full font-medium text-white bg-[#0047FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium  text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
                 </Link>
                 <a
                   href="#"
@@ -87,7 +129,7 @@ function LoginFrom() {
                   </a>
                 </p>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
--- pages/singup/index.js
+++ pages/singup/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import img from "../../public/images/logo.png";
 import Image from "next/image";
-import Link from "next/link";
-function SignUp({ setLogin }) {
-  console.log(setLogin, "condation")
+function index() {
   return (
     <div>
       <div className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white  shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <div className="space-y-4 md:space-y-6">
+              <form className="space-y-4 md:space-y-6" action="#">
                 <div className="flex justify-center my-14">
                   <Image src={img} alt="image" />
                 </div>
@@ -27,7 +25,7 @@ function SignUp({ setLogin }) {
                 </div>
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Email & Phone number
+                    Phone number
                   </label>
                   <input
                     type="number"
@@ -65,13 +63,12 @@ function SignUp({ setLogin }) {
                 <div className="flex items-center justify-between">
                   <div className="flex items-start"></div>
                 </div>
-                <Link href={"/form"}>
-                  <button
-                    className="w-full flex items-center justify-center font-medium text-white  bg-[#0047FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium  text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                  >
-                    Sign up
-                  </button>
-                </Link>
+                <button
+                  type="submit"
+                  className="w-full flex items-center justify-center font-medium text-white  bg-[#0047FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium  text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign up
+                </button>
                 <p className="text-sm font-light flex justify-center  text-gray-500 dark:text-gray-400">
                   Already have an account?
                   <a
@@ -81,7 +78,7 @@ function SignUp({ setLogin }) {
                     Log in
                   </a>
                 </p>
-              </div>
+              </form>
             </div>
           </div>
         </div>
@@ -90,4 +87,4 @@ function SignUp({ setLogin }) {
   );
 }
 
-export default SignUp;
+export default index;
